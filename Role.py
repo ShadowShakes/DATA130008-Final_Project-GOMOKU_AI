b97be201c5f8422e9e8f9479b64db4164945d4a8
ShadowShakes <58924528+ShadowShakes@users.noreply.github.com>
2021-04-16 13:09:56 +0800

    Add files via upload

---
+++ Role.py
@@ -0,0 +1,15 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/12/4 19:26
+# @Author : 詹远瞩,陈劭涵
+# @File : Role.py
+# @Software : PyCharm
+
+# no piece on this location
+EMPTY = 0
+# our AI
+ME = 1
+# the opponent
+OP = 2
+
+
+
